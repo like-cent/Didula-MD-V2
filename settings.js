d2bd82633a645846df47d460ef31089d0245b515
like-cent <pasindulakshan3599@gmail.com>
2025-02-21 20:32:17 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -6,12 +6,12 @@ function convertToBool(text, fault = 'true') {
 }
 
 module.exports = {
-    SESSION_ID: process.env.SESSION_ID === undefined ? 'put your session id' : process.env.SESSION_ID,
+    SESSION_ID: process.env.SESSION_ID === undefined ? 'PRABATH-MD~Ad5QCY7S#1v_cKtWT0rRRGgrEuXR3UZpSXwYhvxkjmVfkYoyIRn8' : process.env.SESSION_ID,
     PREFIX: process.env.PREFIX || '.',
     FOOTER: process.env.FOOTER || "*✦𝐃𝐢𝐝𝐮𝐥𝐚 𝐌𝐃 𝐕𝟐✦*",
     ALIVE_IMG: process.env.ALIVE_IMG || "https://i.ibb.co/tC37Q7B/20241220-122443.jpg",
     PORT: process.env.PORT === undefined ? "8000" : process.env.PORT,
-HEART_REACT: process.env.HEART_REACT || 'false',
+HEART_REACT: process.env.HEART_REACT || 'true',
     SUDO: process.env.SUDO === undefined ? '9473737373,9363636367' : process.env.SUDO,
     ALIVE_MSG: process.env.ALIVE_MSG === undefined ? "*Hello, I am alive now!!*" : process.env.ALIVE_MSG,
     AUTO_READ_STATUS: process.env.AUTO_READ_STATUS === undefined ? "true" : process.env.AUTO_READ_STATUS,
@@ -21,7 +21,7 @@ HEART_REACT: process.env.HEART_REACT || 'false',
     ANTI_CALL: process.env.ANTI_CALL === undefined ? "false" : process.env.ANTI_CALL,
     ANTI_DELETE: process.env.ANTI_DELETE === undefined ? "false" : process.env.ANTI_DELETE,
     ANTI_BOT: process.env.ANTI_BOT === undefined ? "false" : process.env.ANTI_BOT,
-    READ_CMD: process.env.READ_CMD === undefined ? "false" : process.env.READ_CMD,
+    READ_CMD: process.env.READ_CMD === undefined ? "true" : process.env.READ_CMD,
     RECORDING: process.env.RECORDING === undefined ? "false" : process.env.RECORDING,
     AI_CHAT: process.env.AI_CHAT === undefined ? "false" : process.env.AI_CHAT,
     AUTO_SONG_SENDER: process.env.AUTO_SONG_SENDER === undefined ? "false" : process.env.AUTO_SONG_SENDER
